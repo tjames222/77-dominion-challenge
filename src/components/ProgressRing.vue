bf01d3dc94ca2ead9dff455689ed8020b6d58175
Tim James <32233010+tjames222@users.noreply.github.com>
2026-06-29 15:05:36 -0700

    Add progress ring component

---
+++ src/components/ProgressRing.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{ value: number; label: string; sublabel: string }>();
+</script>
+
+<template>
+  <div class="ring" :style="{ '--value': `${value}%` }">
+    <div class="ring-inner">
+      <strong>{{ value }}%</strong>
+      <span>{{ label }}</span>
+      <small>{{ sublabel }}</small>
+    </div>
+  </div>
+</template>
