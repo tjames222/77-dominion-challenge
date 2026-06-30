cce9f99f8948d049339be55d9ba54a74fee0b69d
Tim James <32233010+tjames222@users.noreply.github.com>
2026-06-29 22:50:45 -0700

    Configure Vite multi page build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   base: '/77-dominion-challenge/',
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        register: resolve(__dirname, 'register.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        science: resolve(__dirname, 'science.html'),
+      },
+    },
+  },
 });
